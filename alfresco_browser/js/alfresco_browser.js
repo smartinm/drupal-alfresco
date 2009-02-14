068758a3e1660b26121a44e2579a80fa75d79385
Sergio Martín Morillas <smartinm@users.noreply.github.com>
2009-02-14 18:29:55 +0000

    Cambios referentes al Ticket #118

--- alfresco_browser/js/alfresco_browser.js
+++ alfresco_browser/js/alfresco_browser.js
@@ -1,99 +1,82 @@
-/*
- * Ext JS Library 2.2.1
- * Copyright(c) 2006-2009, Ext JS, LLC.
- * licensing@extjs.com
- * 
- * http://extjs.com/license
- */
+// $Id$
 
 //
-// This is the main layout definition.
+// Main layout definition.
 //
-Ext.onReady(function(){
-	
-	Ext.QuickTips.init();
-	
-	//The default start page, also a simple example of a FitLayout.
-	var start = {
-	  id: 'start-panel',
-	  title: 'Contenido',
-	  layout: 'fit',
-	  bodyStyle: 'padding:25px',
-	  contentEl: 'start-div'  // pull existing content from the page
-	};
+Ext.onReady(function() {
 
-	// This is the main content center region that will contain each example layout panel.
-	// It will be implemented as a CardLayout since it will contain multiple panels with
-	// only one being visible at any given time.
-	var contentPanel = {
-		id: 'content-panel',
-		region: 'center', // this is what makes this panel into a region within the containing layout
-		layout: 'card',
-		margins: '2 5 5 0',
-		activeItem: 0,
-		border: false,
-		items: [
-			start
-		]
-	};
-    
-	// Go ahead and create the TreePanel now so that we can use it below
-    var treePanel = new Ext.tree.TreePanel({
-    	id: 'tree-panel',
-    	title: 'Espacios',
-        region:'north',
-        split: true,
-        height: 300,
-        minSize: 150,
-        autoScroll: true,
-        
-        // tree-specific configs:
-        rootVisible: false,
-        lines: false,
-        singleExpand: true,
-        useArrows: true,
-        
-        loader: new Ext.tree.TreeLoader({
-            dataUrl:'tree-data.json'
-        }),
-        
-        root: new Ext.tree.AsyncTreeNode()
-    });
-    
-	// This is the Details panel that contains the description for each example layout.
-	var detailsPanel = {
-		id: 'details-panel',
-        title: 'Detalles',
-        region: 'center',
-        bodyStyle: 'padding-bottom:15px;background:#eee;',
-		autoScroll: true,
-		html: '<p class="details-info">Información del fichero</p>'
-    };
-	
-	// Finally, build the main layout once all the pieces are ready.  This is also a good
-	// example of putting together a full-screen BorderLayout within a Viewport.
-    new Ext.Viewport({
-		layout: 'border',
-		title: 'Ext Layout Browser',
-		items: [{
-			xtype: 'box',
-			region: 'north',
-			applyTo: 'header',
-			height: 30
-		},{
-			layout: 'border',
-	    	id: 'layout-browser',
-	        region:'west',
-	        border: false,
-	        split:true,
-			margins: '2 0 5 5',
-	        width: 275,
-	        minSize: 100,
-	        maxSize: 500,
-			items: [treePanel, detailsPanel]
-		},
-			contentPanel
-		],
-        renderTo: Ext.getBody()
-    });
+  //Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+
+  Ext.QuickTips.init();
+
+  var treePanel = new Ext.tree.TreePanel({
+    id            : 'tree-panel',
+    title         : 'Browse Spaces',
+    region        : 'north',
+    split         : true,
+    height        : 300,
+    minSize       : 150,
+    autoScroll    : true,
+
+    // tree-specific configs:
+    rootVisible   : false,
+    //lines         : false,
+    //singleExpand  : true,
+    useArrows     : true,
+
+    loader        : new Ext.tree.TreeLoader({
+      dataUrl : 'alfresco/browser/dummy'
+    }),
+
+    root          : new Ext.tree.AsyncTreeNode({
+      id:'xxx'
+    })
+  });
+
+  var detailsPanel = {
+    id :'details-panel',
+    title :'Details',
+    region :'center',
+    bodyStyle :'padding-bottom:15px;background:#eee;',
+    autoScroll :true,
+    html :'<p class="details-info">Información del fichero</p>'
+  };
+
+  new Ext.Viewport({
+    layout : 'border',
+    items  : [{
+      //xtype :'box',
+      region :'north',
+      height : 35,
+      //autoEl: {tag:'div', id:'header', html:'<h1>Alfresco Browser</h1>'}
+      html : '<div id="header"><h1>Alfresco Node Browser for Drupal</h1></div>'
+    }, {
+      layout :'border',
+      id :'layout-browser',
+      region :'west',
+      border :false,
+      split :true,
+      margins :'2 0 5 5',
+      width :275,
+      minSize :100,
+      maxSize :500,
+      items : [ treePanel, detailsPanel ]
+    }, {
+      id :'content-panel',
+      title :'Content Items',
+      region :'center', // this is what makes this panel into a region within
+      layout :'fit',
+      margins :'2 5 5 0',
+      //border :false,
+      bodyStyle :'padding:25px',
+      bbar: new Ext.StatusBar({
+        defaultText   : 'Default status',
+        id            : 'basic-statusbar',
+        items         : [{
+          text: 'A Button'
+        }, '-', 'Plain Text', ' ', ' ']
+      })
+    }],
+    renderTo : Ext.getBody()
+  });
 });
