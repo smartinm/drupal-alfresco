b9873127a1476660b37f5c39dc1787aac02b51c3
Sergio Martín Morillas <smartinm@users.noreply.github.com>
2010-02-03 11:47:11 +0000

    Ticket #304: Preparar publicación en Drupal.org

--- alfresco.js
+++ alfresco.js
@@ -1,7 +1,6 @@
 // $Id$
 
 Drupal.behaviors.alfresco = function (context) {
-  
   $('input#edit-use-title').click(function() {
     if (this.checked) {
       $('#edit-title').attr('readonly', 'readonly');
