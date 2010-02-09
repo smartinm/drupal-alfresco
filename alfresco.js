1a8230889e71a82c1b2747bbeb0fc07c0250d1ad
Sergio Martín Morillas <smartinm@users.noreply.github.com>
2010-02-09 22:37:04 +0000

    Ticket #130: Añadir contador de descargas

--- alfresco.js
+++ alfresco.js
@@ -2,3 +2,15 @@
 
 Drupal.behaviors.alfresco = function (context) {
 }
+
+Drupal.behaviors.alfrescoSettings = function (context) {
+  var passwordInput = $('#edit-alfresco-credentials-password');
+  if (!passwordInput.val()) {
+    passwordInput.val('/DrupalWTF/');
+  }
+  $('#edit-submit').click(function() {
+    if (passwordInput.val() == '/DrupalWTF/') {
+      passwordInput.val('');
+    }
+  });
+}
